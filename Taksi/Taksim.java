4af0d8bb091f6d1a78f491d7871aec458bbe0ed9
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-06 17:17:38 +0300

    ilk

---
+++ Taksi/Taksim.java
@@ -0,0 +1,14 @@
+package Taksi;
+import java.util.Scanner;
+public class Taksim {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("lütfen gidilen mesafeyi bilirtiniz(km) ");
+        double km = in.nextDouble();
+        double ucret = km*(2.20);
+        double ucretİki=ucret+10;
+        double ucretUc= ucretİki>20?ucretİki:20;
+        System.out.println("ödemeniz gereken ücret : "+ ucretUc);
+    }
+    
+}
