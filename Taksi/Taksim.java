c49faa876bc2d6f4a1a4fc94464ba60b89c380cb
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-07 13:53:55 +0300

    loginForm komiti

--- Taksi/Taksim.java
+++ Taksi/Taksim.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Taksim {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("lütfen gidilen mesafeyi bilirtiniz(km) ");dfv
+        System.out.println("lütfen gidilen mesafeyi bilirtiniz(km) ");
         double km = in.nextDouble();
         double ucret = km*(2.20);
         double ucretİki=ucret+10;
         double ucretUc= ucretİki>20?ucretİki:20;
+        System.out.println("merhaba ben loginForm");
         System.out.println("ödemeniz gereken ücret : "+ ucretUc);
     }
     
