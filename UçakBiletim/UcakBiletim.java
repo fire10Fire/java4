ce8adc555989b9b8f6c520561d5e71828eab89ab
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-12 15:34:41 +0300

    indirimler eklendi

--- UçakBiletim/UcakBiletim.java
+++ UçakBiletim/UcakBiletim.java
@@ -6,6 +6,8 @@ public class UcakBiletim {
     Scanner in = new Scanner(System.in);
     int yas, mesafe, tip;
     boolean tip2= false;
+    double yaşIn=0;
+    double digeri=0; 
     
     System.out.println("yaşınız : ");
     yas = in.nextInt();
@@ -23,16 +25,22 @@ public class UcakBiletim {
 if(tip2){
     if(yas<12){
         ucret= ucret-(ucret*1/2);
+        yaşIn=(ucret*1/2);
     }  
     else if(yas<24){
         ucret = ucret-(ucret*1/10);
+        yaşIn=ucret*1/10;
     }
     else if(yas>65){
         ucret= ucret-(ucret*3/10);
+        yaşIn=ucret*3/10;
     }
     if(tip==2){
         ucret=ucret-(ucret*2/10);
+        digeri=ucret*2/10;
     }
+    System.out.println("yaş indirimi : "+yaşIn);
+    System.out.println("çift bilet indirimi : "+digeri);
     System.out.println("lütfen ödeme yapınız : "+ucret);
 }else {
 System.out.println("yanlış seçim yaptınız!");
